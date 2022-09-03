901ac3d8dc6fffd4120861b8d52e3f26bc87cc05
maxkatkalov <baseynice@gmail.com>
2022-09-04 02:13:05 +0300

    New DB for Heroku deployment

--- Board_App/settings.py
+++ Board_App/settings.py
@@ -76,9 +76,21 @@ WSGI_APPLICATION = 'Board_App.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'postgres',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'Baseynice1@',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
--- post/models.py
+++ post/models.py
@@ -7,3 +7,10 @@ class Post(models.Model):  # new
 
     def __str__(self):
         return self.text[:50]
+
+
+class Msg(models.Model):  # new
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:50]

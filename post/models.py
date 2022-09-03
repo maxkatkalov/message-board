5ae72de5eb4054b45b0d1b08459d55538a32c914
maxkatkalov <baseynice@gmail.com>
2022-09-04 01:03:00 +0300

    initial commit

---
+++ post/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.db import models
+
+
+class Post(models.Model):  # new
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:50]

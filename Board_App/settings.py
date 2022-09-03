1f4cdbfdcf28a57a431ed39e054b0144a5ba748b
maxkatkalov <baseynice@gmail.com>
2022-09-04 01:34:19 +0300

    New updates for Heroku deployment

--- Board_App/settings.py
+++ Board_App/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+ajf#%xeww=)k*s9$2%ofm-x$pa=hk@*$qxiz32ec#09b_^*v%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
